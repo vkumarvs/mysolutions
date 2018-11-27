36c704a4321bf55738d14f8d5572ca85316cb134
vkumarvs <vip83.dha@gmail.com>
2018-11-27 11:04:33 +0100

    cleaning up master branch content

--- README.md
+++
@@ -1,4 +0,0 @@
-mysolutions
-===========
-
-First attempt to get hands on for my future solutions :)
--- poco_learning/compile.sh
+++
@@ -1,1 +0,0 @@
-g++ PocoThreadExample.cpp  -o poco_test /home/vipin/VIEW/performance_main/lib/libPocoFoundation.so /home/vipin/VIEW/performance_main/lib/libPocoUtil.so
