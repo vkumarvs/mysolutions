4c8d81cfeceb4fb066ff0b338cfd9bf201866ed1
vkumarvs <vip83.dha@gmail.com>
2018-12-05 11:09:00 +0100

    Another cipher test program

---
+++ cipher_verification/pyssl.sh
@@ -0,0 +1,1 @@
+g++ -std=c++0x PytestSSL.cpp -L /home/MSD/POCO_LEARNING/poco_install/lib -L /usr/lib64/ -I /home/MSD/POCO_LEARNING/poco_install/include -I. -lPocoNet -lPocoNetSSL -lPocoCrypto -lPocoUtil -lPocoFoundation -lcrypto
