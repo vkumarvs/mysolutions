151ae23e5a86c7431bf440e2ac3a4b5db5c80f71
vkumarvs <vip83.dha@gmail.com>
2018-12-05 10:58:51 +0100

    simple cipher program

---
+++ cipher_verification/compile.sh
@@ -0,0 +1,2 @@
+#g++ Mp4Dump.cpp poco_http_server.cpp -o pocoserver -L /home/vipin/VIEW/performance_main/lib/ -I. -I Bento4/include/ -L Bento4/lib/ -lAP4 -lPocoNet -lPocoUtil -lPocoFoundation -lfmp4
+g++ -std=c++0x cipher_memory.cpp -L /home/MSD/POCO_LEARNING/poco_install/lib -L /usr/lib64/ -I /home/MSD/POCO_LEARNING/poco_install/include -I. -lPocoNet -lPocoNetSSL -lPocoCrypto -lPocoUtil -lPocoFoundation -lcrypto
